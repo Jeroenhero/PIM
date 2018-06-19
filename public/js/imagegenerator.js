3a3af0fee2856c5f98d72da7817e48e7dbc28d53
Jeroen <hotgamecrew@gmail.com>
2018-06-19 14:51:44 +0200

    Muppets

--- public/js/imagegenerator.js
+++ public/js/imagegenerator.js
@@ -1,15 +1,73 @@
+function test() {
+    var numbers = [1,2,3,4,5,6,7,8,9,10,11];
+    setupHeaderImage(numbers)
+}
 
-function setupHeaderImage() {
+function setupHeaderImage(numbers) {
     var canvas = document.getElementById("myCanvas");
     var ctx = canvas.getContext("2d");
     base_image = new Image();
     base_image.src = 'rijksmuseumlegenda.png';
     base_image.onload = function(){
         ctx.drawImage(base_image, 0, 0);
-        ctx.beginPath();
-        ctx.arc(450,303,13,0,2*Math.PI);
-        ctx.fillStyle = "red";
-        ctx.fill();
-        ctx.stroke();
+        for(i = 0; i < numbers.length; i++) {
+            var location = getHeaderPartLocation(numbers[i])
+            ctx.beginPath();
+            ctx.arc(location.x, location.y, 13,0,2*Math.PI);
+            ctx.fillStyle = "red";
+            ctx.fill();
+            ctx.stroke();
+        }
+    }
+}
+
+function getHeaderPartLocation(number) {
+    if(number == 1) {
+        var location = {x: 443, y: 253}
+        return location;
+    }
+    if(number == 2) {
+        var location = {x: 443, y: 303}
+        return location;
+    }
+    if(number == 3) {
+        var location = {x: 443, y: 348}
+        return location;
+    }
+    if(number == 4) {
+        var location = {x: 443, y: 380}
+        return location;
+    }
+    if(number == 5) {
+        var location = {x: 559, y: 226}
+        return location;
+    }
+    if(number == 6) {
+        var location = {x: 651, y: 303}
+        return location;
+    }
+    if(number == 7) {
+        var location = {x: 743, y: 226}
+        return location;
+    }
+    if(number == 8) {
+        var location = {x: 858, y: 253}
+        return location;
+    }
+    if(number == 9) {
+        var location = {x: 858, y: 303}
+        return location;
+    }
+    if(number == 10) {
+        var location = {x: 858, y: 346}
+        return location;
+    }
+    if(number == 11) {
+        var location = {x: 858, y: 378}
+        return location;
+    }
+    else {
+        var location = {x: 10000, y: 30000}
+        return location;
     }
 }
