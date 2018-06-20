525be319d42586948650fc566e5d6ca2f9af3688
sebas <sebastiaanspanjer@gmail.com>
2018-06-20 12:46:21 +0200

    Sebas toegevoegd

--- public/js/photogrid.js
+++ public/js/photogrid.js
@@ -1,5 +1,32 @@
+var selected=[];
+var buttonValid=false;
+
 function selectImage(container) {
-    alert(container);
-    document.getElementById(container).style.border = '5px solid #021a40';
-    document.getElementById(container).style.padding = '1';
+    var containerName = "container" + container;
+    if(selected.includes(container)) {
+        document.getElementById(containerName).style.border = '0';
+        document.getElementById(containerName).style.padding = '0';
+        var index = selected.indexOf(container);
+        if (index>-1){
+            selected.splice(index,1)
+        }
+    }
+    else {
+        document.getElementById(containerName).style.border = '5px solid #021a40';
+        document.getElementById(containerName).style.padding = '1';
+        selected.push(container);
+
+    }
+    testButtonValidity();
+}
+function testButtonValidity(){
+    if (selected.length > 0 && buttonValid == false){
+        buttonValid = true
+        document.getElementById("confirmbutton").style.backgroundColor ='#4CAF50';
+
+    }
+    else if( selected.length == 0 && buttonValid == true){
+        buttonValid = false
+        document.getElementById("confirmbutton").style.backgroundColor ='grey';
+    }
 }
