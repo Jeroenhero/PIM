3643897f5173ec331ee55d38853e6c05a999ef29
Jeroen <hotgamecrew@gmail.com>
2018-06-23 14:18:26 +0200

    Changes to cross-page systems and handling the data.

--- public/js/imagegenerator.js
+++ public/js/imagegenerator.js
@@ -1,81 +1,58 @@
+var paintingNumbers = []
+var headingPartLocations = []
+
 function startUp() {
-    var numbers = [1,2,6,8,9,11];
+    getHeaderPartLocations()
     loadPaintingsFromURL();
-    setupHeaderImage(numbers)
+    setupHeaderImage(paintingNumbers)
+
 }
 
 function loadPaintingsFromURL() {
     var parameters = location.search.substring(1).split("&");
-    alert("Parameters:" +parameters);
     var paintings = parameters[0].split("$")[1];
     alert("PaintingsUnparsed: " + paintings);
+    var paintingsSplit = paintings.split("n");
+    for(var i = 0; i < paintingsSplit.length - 1; i ++) {
+        paintingNumbers.push(parseInt(paintingsSplit[i]));
+    }
 }
 
-function setupHeaderImage(numbers) {
+function setupHeaderImage() {
     var canvas = document.getElementById("myCanvas");
     var ctx = canvas.getContext("2d");
     base_image = new Image();
     base_image.src = 'rijksmuseumlegenda.png';
     base_image.onload = function(){
         ctx.drawImage(base_image, 0, 0);
-        for(i = 0; i < numbers.length; i++) {
-            var location = getHeaderPartLocation(numbers[i])
-            ctx.beginPath();
-            ctx.arc(location.x, location.y, 13 ,0,2*Math.PI);
-            ctx.fillStyle = "red";
-            ctx.fill();
-            ctx.stroke();
+        for(var i = 0; i < headingPartLocations.length; i ++) {
+            location - headingPartLocations[i];
+            if(paintingNumbers.includes(location.id)) {
+                ctx.beginPath();
+                ctx.arc(location.x, location.y, 13 ,0,2*Math.PI);
+                ctx.fillStyle = "red";
+                ctx.fill();
+                ctx.stroke();
+            }
         }
     }
 }
 
-function getHeaderPartLocation(number) {
-    if(number == 1) {
-        var location = {x: 443, y: 253}
-        return location;
+function getHeaderPartLocations() {
+    var request = new XMLHttpRequest();
+    request.open("GET", "data/buildinglocations.json", true);
+    request.send(null);
+    request.onreadystatechange = function() {
+        if ( request.readyState === 4 && request.status === 200 ) {
+            var locations = JSON.parse(request.responseText);
+            console.log(locations);
+            for(var i = 0; i < locations.length; i ++) {
+                var location = locations[i];
+                var locObj = {id:parseInt(location.id), x: parseInt(location.x), y:parseInt(location.y)};
+                headingPartLocations.push(locObj);
+            }
+        }
     }
-    if(number == 2) {
-        var location = {x: 443, y: 303}
-        return location;
-    }
-    if(number == 3) {
-        var location = {x: 443, y: 348}
-        return location;
-    }
-    if(number == 4) {
-        var location = {x: 443, y: 380}
-        return location;
-    }
-    if(number == 5) {
-        var location = {x: 559, y: 226}
-        return location;
-    }
-    if(number == 6) {
-        var location = {x: 651, y: 303}
-        return location;
-    }
-    if(number == 7) {
-        var location = {x: 743, y: 226}
-        return location;
-    }
-    if(number == 8) {
-        var location = {x: 858, y: 253}
-        return location;
-    }
-    if(number == 9) {
-        var location = {x: 858, y: 303}
-        return location;
-    }
-    if(number == 10) {
-        var location = {x: 858, y: 346}
-        return location;
-    }
-    if(number == 11) {
-        var location = {x: 858, y: 378}
-        return location;
-    }
-    else {
-        var location = {x: 10000, y: 30000}
-        return location;
-    }
+    console.log("Parsed " + headingPartLocations.length + " locations!")
+
 }
